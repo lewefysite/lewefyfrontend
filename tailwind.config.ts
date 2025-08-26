14f0148ec9133482936dd8be4afaee5aaa75b6d2
lewefysite <felipecaxeiro@gmail.com>
2025-08-26 12:29:32 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
-import { type Config } from 'tailwindcss'
-
-const config: Config = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
 
-export default config
