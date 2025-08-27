cb274e540a3b320df7af8c5801068cfc5ba93a67
lewefysite <felipecaxeiro@gmail.com>
2025-08-27 12:35:28 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import ScrollToTop from './utils/scrolltotop.jsx';
+import ScrollToTop from './util/scrolltotop.jsx';
 import HomePage from './pages/homepage.jsx';
 import NotFoundPage from './pages/notfoundpage.jsx';
 
