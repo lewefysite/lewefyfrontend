108d5ab398f6ea2694d8a4bf413a70d138b7712e
lewefysite <felipecaxeiro@gmail.com>
2025-08-27 00:11:28 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,31 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './public/**/*.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandprimary: 'rgba(76, 175, 79, 1)',
+        brandinfo: 'rgba(33, 148, 243, 1)',
+        brandsecondary: 'rgba(38, 50, 56, 1)',
+        neutralwhite: 'rgba(255, 255, 255, 1)',
+        actionerror: 'rgba(229, 56, 53, 1)',
+        actionsuccess: 'rgba(46, 125, 49, 1)',
+        actionwarning: 'rgba(251, 192, 45, 1)',
+      },
+      fontFamily: {
+        inter: ['Inter', 'Helvetica', 'sans-serif'],
+        roboto: ['Roboto', 'Helvetica', 'sans-serif'],
+        arimo: ['Arimo', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
 
+export default config;
